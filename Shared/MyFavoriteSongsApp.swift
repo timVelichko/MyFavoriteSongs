1e0aa875488492fae9cb31208a096f381b57305e
Tim Velichko <timvelichko@gmail.com>
2022-06-16 12:58:42 +0200

    Initial Commit

---
+++ Shared/MyFavoriteSongsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavoriteSongsApp.swift
+//  Shared
+//
+//  Created by Tim Velichko on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFavoriteSongsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
